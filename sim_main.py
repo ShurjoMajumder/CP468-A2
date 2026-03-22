2827fb1957f1c390bd9af3bebe47acbc8caa8638
ShurjoMajumder <shurjo.majumder@gmail.com>
2026-03-22 19:50:05 -0400

    grid search and improved agent parameters added to sim_main.py

--- sim_main.py
+++ sim_main.py
@@ -8,8 +8,21 @@ if __name__ == '__main__':
 
     # world.draw_street_graph()  # uncomment this line to view the graph representing the roads
 
+    # initialize the agent
     # the starting position can be any integer in the closed interval [0, 15].
-    agent = Agent(start=0, dest=15, max_walking_dist=3.0)
+    agent_params = {
+        "start": 0,
+        "dest": 15,
+        "max_walking_dist": 3.0,
+        "alpha": 0.2,
+        "gamma": 0.8,
+        "epsilon": 0.6,
+        "epsilon_decay": 0.99,
+        "epsilon_min": 0.01,
+    }
+    agent = Agent(**agent_params)
+
+    # Set this variable to `False` to use the dijkstra-based agent.
     use_q_learning = True
 
     print(f"[{datetime.datetime.now().astimezone()}] Simulation started.")
